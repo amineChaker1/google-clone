49d5df2f1505165d2286a5315ce7ed2e057ec593
Chaker Amine <chkramine2003@gmail.com>
2023-03-11 04:47:04 +0100

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
+import workoutRouter from "./routes/workouts.js";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+app.use(express.json());
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
+app.use("/workouts", workoutRouter);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 app.listen(3000, () => {
   console.log("server started at 3000");
 });
